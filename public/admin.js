13cfa6530952fc19d1e75bbf0470ec3e00240121
artem1248 <artem.boiko.boiko.42@gmail.com>
2026-07-16 18:37:31 +0300

    Remove initPhotos function from admin.js
    Removed the initPhotos function that handles photo upload interactions.

--- public/admin.js
+++ public/admin.js
@@ -152,31 +152,6 @@ hidden>
 initPhotos();
 
 }
-function initPhotos(){
-
-    const addButton = document.getElementById("addPhoto");
-    const input = document.getElementById("photoInput");
-    const list = document.getElementById("photoList");
-
-    addButton.addEventListener("click",()=>{
-
-        input.click();
-
-    });
-
-    input.addEventListener("change",(e)=>{
-
-        const files=[...e.target.files];
-
-        list.innerHTML="";
-
-        if(files.length===0){
-
-            list.innerHTML="<p>Поки що фотографій немає 🐾</p>";
-
-            return;
-
-        }
 
         files.forEach(file=>{
 
