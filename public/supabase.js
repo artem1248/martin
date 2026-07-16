ca5e09d092b70122bd91cd548c402a2fba1395ba
artem1248 <artem.boiko.boiko.42@gmail.com>
2026-07-16 17:46:54 +0300

    Update Supabase URL to remove '/rest/v1/'

--- public/supabase.js
+++ public/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://jnudjbfcvrwibgpkprpj.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://jnudjbfcvrwibgpkprpj.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_0elEvOKlR0vpAN6_rvEBNQ_Z212yvgD";
 
 const supabase = window.supabase.createClient(
